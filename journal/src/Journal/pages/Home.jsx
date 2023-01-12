7083c17e016b8e5e8d9c392e817e82fb43d9c340
Gonzalo <102692082+gnz6@users.noreply.github.com>
2023-01-12 16:35:54 -0300

    home

--- journal/src/Journal/pages/Home.jsx
+++ journal/src/Journal/pages/Home.jsx
@@ -1,14 +1,36 @@
+import { IconButton } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
-import { Typography } from '@mui/material'
+import { NoteView } from '../view/NoteView'
+import { NothingSelecterView } from '../view/NothingSelecterView'
+import { AddOutlined } from '@mui/icons-material'
 
 
 
 export const Home = () => {
   return (
     <JournalLayout>
-      <Typography>
+      <NothingSelecterView/>
         
-      </Typography>
+      <NoteView/>
+
+      <IconButton
+      
+        size='large'
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 40, 
+          bottom: 30,
+          zIndex:10,
+          padding: 2
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+
+      </IconButton>
+
     </JournalLayout>
   )
 }
